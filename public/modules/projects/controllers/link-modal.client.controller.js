b1522085a431ec7175d6752045c12de20a07f932
ppoplawska <patrickpoplawska@gmail.com>
2015-12-09 00:58:35 -0500

    Refactored link-modal controller, and deed comments

--- public/modules/projects/controllers/link-modal.client.controller.js
+++ public/modules/projects/controllers/link-modal.client.controller.js
@@ -1,3 +1,6 @@
+//This is the controller for our project-modal view. Its main function is to link the selected
+//project into the modal view
+
 'use strict';
 
 angular.module('projects').controller('LinkModalController', ['$scope', '$modalInstance', '$sce', 'Projects', 'prjId', 'eleId',
@@ -6,39 +9,38 @@ angular.module('projects').controller('LinkModalController', ['$scope', '$modalI
 	$scope.sc = $sce;  // html security for ckeditor text
 	  // find project in the database
 	$scope.elementId = eleId;  // save element id (index)
-	$scope.projectTest = 'this is the project test';
+	$scope.selectedProject = 'this is the project test';
 
-	$scope.resultArray = false;
+	$scope.resultArray = false; //some default dummy value for resultArray
+
+	//This function is called by the modal view, it serves the purpose of populating the view elements based on the selection
 	$scope.lookupElement = function(){
-		var result;
-		console.log('got into the function');
-		console.log('the value of projectId is:');
-		console.log(prjId);
-		$scope.projectTest = Projects.get({ projectId: prjId },function(){
-				$scope.project = $scope.projectTest;
-				console.log($scope.projectTest.elements.length);
+				var result;
+				//utilize a callback, so that the Project.get (async) finishes before the view loads
+				$scope.selectedProject = Projects.get({ projectId: prjId },function(){
+				$scope.project = $scope.selectedProject;
+				console.log($scope.selectedProject.elements.length);
 				if(!eleId) {
 					console.log('EleId undefined');
-					$scope.resultArray = 	$scope.projectTest.elements;
+					$scope.resultArray = 	$scope.selectedProject.elements;
 					console.log($scope.resultArray);
-					return $scope.projectTest.elements;
+					return $scope.selectedProject.elements;
 				} else {
-					for(var i=0; i < $scope.projectTest.elements.length; i++ )
+					for(var i=0; i < $scope.selectedProject.elements.length; i++ )
 					{
-						if($scope.projectTest.elements[i]._id === $scope.elementId ){
-								var newArray = [$scope.projectTest.elements[i]];
+						if($scope.selectedProject.elements[i]._id === $scope.elementId ){
+							//if the user selected a specfic element, then we need to return it as a collection
+							//for processing by ng-repeat
+								var newArray = [$scope.selectedProject.elements[i]];
 								$scope.resultArray = 	newArray;
 
-								return 	$scope.projectTest.elements[i];
+								return 	$scope.selectedProject.elements[i];
 
-						}else{
-								console.log('No result was found');
 						}
 					}
 				}
 
-				//$scope.resultArray = false;
-				//return false;
+				
 
 
 
