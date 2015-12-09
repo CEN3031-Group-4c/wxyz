024a70b8c7d074a08ee9e54744a70fa7e30eba22
David Campbell <dcampbell82@ufl.edu>
2015-12-08 19:23:38 -0500

    Account Security Improvement: Only Professors can create accounts for other Professors #108686390

--- app/controllers/users/users.authentication.server.controller.js
+++ app/controllers/users/users.authentication.server.controller.js
@@ -12,6 +12,43 @@ var _ = require('lodash'),
 /**
  * Signup
  */
+exports.initialsignup = function(req, res) {
+	// For security measurement we remove the roles from the req.body object
+	delete req.body.roles;
+
+	// Init Variables
+	var user = new User(req.body);
+	var message = null;
+
+	// Add missing user fields
+	user.provider = 'local';
+	user.displayName = user.firstName + ' ' + user.lastName;
+
+	// Then save the user
+	user.save(function(err) {
+		if (err) {
+			return res.status(400).send({
+				message: errorHandler.getErrorMessage(err)
+			});
+		} else {
+			// Remove sensitive data before login
+			user.password = undefined;
+			user.salt = undefined;
+
+			req.login(user, function(err) {
+				if (err) {
+					res.status(400).send(err);
+				} else {
+					res.json(user);
+				}
+			});
+		}
+	});
+};
+
+/**
+ * Signup
+ */
 exports.signup = function(req, res) {
 	// For security measurement we remove the roles from the req.body object
 	delete req.body.roles;
--- public/modules/users/controllers/authentication.client.controller.js
+++ public/modules/users/controllers/authentication.client.controller.js
@@ -1,4 +1,4 @@
-'use strict'; 
+'use strict';
 
 angular.module('users').controller('AuthenticationController', ['$scope', '$http', '$location', 'Authentication',
 	function($scope, $http, $location, Authentication) {
